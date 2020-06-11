5367335f229ba418fbad61dcca35866671369de9
kush-channawar <64893595+kush-channawar@users.noreply.github.com>
2020-06-11 08:54:17 +0530

    Add files via upload

---
+++ PPL_ASSIGNMENTS/Assignment-2/stack_smashing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+void function(char *str){
+    char buff[16];
+    strcpy(buff, str);
+}
+
+int main()
+{
+    char str[256];
+    for(int i = 0; i < 255; i++) {
+        str[i] = 'a';
+    }
+    function(str);
+
+    return 0;
+}
